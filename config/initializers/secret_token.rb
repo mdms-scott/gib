31dd6728a77eaf03a6b71f90b6d21ded9b4be0c0
mdms-scott <mdms.scott@gmail.com>
2010-11-08 14:04:43 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gib::Application.config.secret_token = '2c03a859e214780d4bc0f861581b1b4e083320c53682e6b7ecd0ef9d7db15ec4978d1357ffb737e6ccf53ef3e2e7ac6d641f3d0c7e42385a8113116d4221ea86'
